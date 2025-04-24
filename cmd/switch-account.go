3137156d088ea7a38bcdb97bb2bad783f777e48a
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-04-24 13:24:24 +0800

    feat: add functionality to switch account

---
+++ cmd/switch-account.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"errors"
+	"os/exec"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+var switchAccountCmd = &cobra.Command{
+	Use:   "switch-account [accountID]",
+	Short: "Switch git account",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if !isInitialized() {
+			cmd.Println("Gitm is not initialized yet, please run gitm init to initialize gitm!")
+			return
+		}
+
+		id := args[0]
+
+		parsedId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			cmd.Println("Invalid account ID (must be integer)")
+			return
+		}
+
+		account, err := viewAccountByID(int(parsedId))
+		if err != nil {
+			if errors.Is(err, ErrAccountNotFound) {
+				cmd.Println("Error: Account not found")
+				return
+			}
+		}
+
+		err = isGitInstalled()
+		if err != nil {
+			if errors.Is(err, ErrGitNotInstalled) {
+				cmd.Println("Error: git not installed")
+				return
+			}
+
+			cmd.Println("Error: ", err)
+			return
+		}
+
+		gitUsernameCmd := exec.Command("git", "config", "--global", "user.name", account.Username)
+		if err := gitUsernameCmd.Run(); err != nil {
+			cmd.Println("Failed to set git username:", err)
+			return
+		}
+
+		gitEmailCmd := exec.Command("git", "config", "--global", "user.email", account.Email)
+		if err := gitEmailCmd.Run(); err != nil {
+			cmd.Println("Failed to set git email:", err)
+			return
+		}
+
+		cmd.Println("Account switched successfully!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(switchAccountCmd)
+}
