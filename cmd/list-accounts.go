bcf7dcc97d04854ad28ee9a404c5bb0c9c6521ea
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-04-25 13:28:39 +0800

    feat: add functionality to display git accounts

---
+++ cmd/list-accounts.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/spf13/cobra"
+)
+
+var (
+	colTitleID       = "ID"
+	colTitleUsername = "Username"
+	colTitleEmail    = "Email"
+	colTitleActive   = "Active"
+	rowHeader        = table.Row{colTitleID, colTitleUsername, colTitleEmail, colTitleActive}
+)
+
+var listAccountsCmd = &cobra.Command{
+	Use:   "list-accounts",
+	Short: "Display list of git accounts",
+	Run: func(cmd *cobra.Command, args []string) {
+		t := table.NewWriter()
+		t.AppendHeader(rowHeader)
+
+		accounts, err := viewAccounts()
+		if err != nil {
+			cmd.Println(err.Error())
+			return
+		}
+
+		for _, account := range accounts {
+			t.AppendRow(table.Row{account.Id, account.Username, account.Email, account.Active})
+		}
+
+		cmd.Println(t.Render())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listAccountsCmd)
+}
